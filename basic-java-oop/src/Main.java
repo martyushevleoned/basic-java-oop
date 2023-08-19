858bb8d6f350fc9012ad4029ac59ec439735a4fc
Martyushev Leoned <56407820+martyushevleoned@users.noreply.github.com>
2023-08-19 14:14:23 +0500

    streams

--- basic-java-oop/src/Main.java
+++ basic-java-oop/src/Main.java
@@ -8,19 +8,46 @@ import figures.Square;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
 //Basic extend
         ArrayList<Figure> figures = new ArrayList<>();
         figures.add(new Circle(5));
+        figures.add(new Square(5));
         figures.add(new Rectangle(29, 1));
+        figures.add(new Rectangle(5, 6));
+        figures.add(new Circle(15));
         figures.add(new Square(12));
+
 //forEach print array
-//        figures.forEach((figure -> System.out.println(figure)));
-        figures.forEach(System.out::println);
+        figures.forEach((figure -> System.out.println(figure + "\t" + figure.getSquare())));
         System.out.println();
 
+//======================================================================================================================
+//Streams
+        figures.stream().sorted(Comparator.comparing(Figure::getSquare)).forEach((figure -> System.out.println(figure + "\t" + figure.getSquare())));
+        System.out.println();
+
+        Optional<Figure> optional = figures.stream().max(Comparator.comparing(Figure::getSquare));
+        optional.ifPresent(System.out::println);
+        System.out.println();
+
+        //сумма всех площадей больше 100
+        figures.stream().map(Figure::getSquare).filter(f -> f > 100).reduce((f1, f2) -> f1 + f2).ifPresent(System.out::println);
+        System.out.println();
+
+        figures.stream().filter(figure -> figure.getSquare() > 100).forEach(System.out::println);
+        System.out.println();
+
+        Stream<Integer> numbers = Stream.of(23, 1, 54, 12, 5, 1, 5, 1, 3, 4, 561, 5, 1, 45, 52, 4, 2);
+        numbers.filter(n -> n % 2 == 0).forEach(n -> System.out.print(n + "\t"));
+        System.out.println();
+        System.out.println();
+
+//======================================================================================================================
 //interface Comparable
         ArrayList<Animal> animals = new ArrayList<>();
         animals.add(new Bird("C The Bird", 100));
